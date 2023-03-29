5ebf1cb44c2317e4b5db8380398a54501d12d1b8
Robert Denis <robert1994denis@gmail.com>
2023-03-29 05:13:42 +0200

    Update MainJugador.java
    casi gris

--- Moviles/app/src/main/java/com/example/myapplication/ejJugador/MainJugador.java
+++ Moviles/app/src/main/java/com/example/myapplication/ejJugador/MainJugador.java
@@ -8,8 +8,6 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
-import android.media.Image;
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -38,11 +36,13 @@ public class MainJugador extends AppCompatActivity {
         idJugador1 = findViewById(R.id.idJugador1);
         idJugador2 = findViewById(R.id.idJugador2);
         idBotonJugar = findViewById(R.id.idBotonJugar);
+
         idJugador1.setTag(R.drawable.help__1_);
         idJugador2.setTag(R.drawable.help__1_);
 
-        Drawable imagenIncognito1 = idJugador1.getDrawable().;
-        Drawable imagenIncognito2 = idJugador1.getDrawable();
+       // int imageId = getResources().getIdentifier("help__1_", "drawable", getPackageName());
+
+
 
 
         ActivityResultLauncher<Intent> lanzadora = registerForActivityResult(
@@ -51,12 +51,13 @@ public class MainJugador extends AppCompatActivity {
                         Intent data = result.getData();
                         String datos = data.getStringExtra("JUGADOR");
                         int noJugador = data.getIntExtra("noJugador", NUMERO_ERROR);
-                        Toast.makeText(this, String.valueOf(noJugador), Toast.LENGTH_SHORT).show();
                         Drawable nuevaImagen = getResources().getDrawable(Integer.parseInt(datos));
                         if(noJugador == NO_JUGADOR1){
                             idJugador1.setImageDrawable(nuevaImagen);
+                            idJugador1.setTag(Integer.parseInt(datos));
                         }else if (noJugador == NO_JUGADOR2){
                             idJugador2.setImageDrawable(nuevaImagen);
+                            idJugador2.setTag(Integer.parseInt(datos));
                         }else{
                             Toast.makeText(this, "No se ha seleccionado jugador", Toast.LENGTH_SHORT).show();
                         }
@@ -70,6 +71,7 @@ public class MainJugador extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(MainJugador.this, SeleccionaJugador.class);
                 intent.putExtra("Jugador",NO_JUGADOR1);
+                intent.putExtra("paraGris", String.valueOf(idJugador2.getTag()));
                 lanzadora.launch(intent);
 
             }
@@ -80,6 +82,7 @@ public class MainJugador extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(MainJugador.this, SeleccionaJugador.class);
                 intent.putExtra("Jugador", NO_JUGADOR2);
+                intent.putExtra("paraGris", String.valueOf(idJugador1.getTag()));
                 lanzadora.launch(intent);
             }
         });
@@ -87,13 +90,23 @@ public class MainJugador extends AppCompatActivity {
         idBotonJugar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(Integer.parseInt(imagen1.toString()) == NO_SELECCIONADO){
-                    Toast.makeText(MainJugador.this, "JUGADOR 1 NO HA SELECCIONADO", Toast.LENGTH_SHORT).show();
-                }else if(Integer.parseInt(imagen2.toString()) == NO_SELECCIONADO){
-                    Toast.makeText(MainJugador.this, "JUGADOR 2 NO HA SELECCIONADO", Toast.LENGTH_SHORT).show();
+                //HAY que parsearlo a string y luego a int
+                String comprueba1 = String.valueOf(idJugador1.getTag());
+                String comprueba2 = String.valueOf(idJugador2.getTag());
+                int comprobador1 = Integer.parseInt(comprueba1);
+                int comprobador2 = Integer.parseInt(comprueba2);
+
+                if (comprobador1 == NO_SELECCIONADO &&  comprobador2 == NO_SELECCIONADO){
+                    Toast.makeText(MainJugador.this, "NO SE HA SELECCIONADO PERSONAJE", Toast.LENGTH_SHORT).show();
+                }else if(comprobador1 == NO_SELECCIONADO){
+                    Toast.makeText(MainJugador.this, "JUGADOR 1 NO SELECCIONADO", Toast.LENGTH_SHORT).show();
+                }else if(comprobador2 == NO_SELECCIONADO){
+                    Toast.makeText(MainJugador.this, "JUGADOR 2 NO SELECCIONADO", Toast.LENGTH_SHORT).show();
                 }else{
-                    Toast.makeText(MainJugador.this, "A JUGAR", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainJugador.this, "A JUGAAR!!!", Toast.LENGTH_SHORT).show();
+
                 }
+
 
 
             }
